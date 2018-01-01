ad8626083e93af3ef06e2facb0ff1b7ba803b486
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-01-01 02:27:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach((driver)=> console.log(driver.name))
+}
+
+//logs the name of each driver whose hometown matches the passed-in argument
+function logDriversByHometown(drivers, hometown) {
+  drivers.forEach((currentDriver)=> {
+    if (currentDriver.hometown === hometown){
+      console.log(currentDriver.name)
+    }
+  })
+}
+
+//uses the sort() method to return a new array of drivers ordered by revenue (lowest to highest)
+function driversByRevenue(drivers) {
+  let driversRevenue = [];
+
+  drivers.forEach((driver)=> {
+    driversRevenue.push(driver)
+  })
+
+  driversRevenue.sort((a, b)=> a.revenue - b.revenue);
+  return driversRevenue
+}
+
+// uses the sort() method to return a new array of drivers ordered alphabetically by name (A to Z)
+function driversByName(drivers) {
+  let driversName = [];
+
+  drivers.forEach((driver) => {
+    driversName.push(driver)
+  });
+
+  driversName.sort(function(a, b){
+    return a.name.localeCompare(b.name);
+  });
+
+  return driversName
+}
+
+//uses the reduce() method to sum the revenue of every driver and return the total
+function totalRevenue(drivers) {
+  let driversRevenue = [];
+
+  drivers.forEach((driver)=> {
+    driversRevenue.push(driver.revenue)
+  })
+
+  const reduceRevenue = function(total, revenue) {
+    console.log(total.revenue + drivers.revenue)
+    return total.revenue + drivers.revenue
+  }
+
+  return driversRevenue.reduce(reduceRevenue)
+
+}
+
+// const array1 = [1, 2, 3, 4];
+// const reducer = (accumulator, currentValue) => accumulator + currentValue;
+//
+// // 1 + 2 + 3 + 4
+// console.log(array1.reduce(reducer));
+// // expected output: 10
+//
+// // 5 + 1 + 2 + 3 + 4
+// console.log(array1.reduce(reducer, 5));
+// // expected output: 15
